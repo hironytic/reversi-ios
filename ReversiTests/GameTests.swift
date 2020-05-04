b42d4af580b52b40e324b895c8dd7991d0196399
Hironori Ichimiya <hiron@hironytic.com>
2020-05-04 15:06:58 +0900

    NextTurnフェーズのテストケースを追加

--- ReversiTests/GameTests.swift
+++ ReversiTests/GameTests.swift
@@ -681,5 +681,148 @@ class GameTests: XCTestCase {
         wait(for: [expectPhaseToBeNextTurn], timeout: 3.0)
         XCTAssertEqual(requestedChanges, cellChanges)
     }
-}
+    
+    func testNextTurnPhase1() throws {
+        let board = try Board(restoringFromText: """
+            o-------
+            -o-o----
+            --oo-o--
+            --xoxox-
+            ---xox--
+            ---ooooo
+            ---o-x--
+            --o-----
+            """)
+        var state = State(board: board, turn: .light, playerModes: [.manual, .manual])
+        state.diskCount[.dark] = 9
+        state.diskCount[.light] = 12
+        state.phase = AnyPhase(NextTurnPhase())
+        
+        let game = Game(state: state, middlewares: [Logger.self])
+        let gameState = game.statePublisher.share()
 
+        // ターンが黒に変わって、ディスク枚数が更新され、
+        // WaitForPlayerフェーズに遷移する
+        let expectTurnChange = expectation(description: "Changed to dark's turn")
+        let stateSubscriber1 = EventuallyFulfill<State, Never>(expectTurnChange, inputChecker: { state in
+            return state.turn == .dark
+        })
+        gameState.subscribe(stateSubscriber1)
+        stateSubscriber1.store(in: &cancellables)
+
+        let expectDiskCount = expectation(description: "Disk-counts change")
+        let stateSubscriber2 = EventuallyFulfill<State, Never>(expectDiskCount, inputChecker: { state in
+            return state.diskCount[.dark] == 6 && state.diskCount[.light] == 16
+        })
+        gameState.subscribe(stateSubscriber2)
+        stateSubscriber2.store(in: &cancellables)
+
+        let expectPhaseToBeWaitForPlayer = expectation(description: "Phase transits to 'WaitForPlayer'")
+        let stateSubscriber3 = EventuallyFulfill<State, Never>(expectPhaseToBeWaitForPlayer, inputChecker: { state in
+            return state.phase.kind == .waitForPlayer
+        })
+        gameState.subscribe(stateSubscriber3)
+        stateSubscriber3.store(in: &cancellables)
+        
+        if let onEnter = NextTurnPhase.onEnter(previousPhase: AnyPhase(PlacingDiskPhase(cellChanges: [][...]))) {
+            game.dispatch(.thunk(onEnter))
+        }
+        wait(for: [expectTurnChange, expectDiskCount, expectPhaseToBeWaitForPlayer], timeout: 3.0)
+    }
+    
+    func testNextTurnPhase2() throws {
+        let board = try Board(restoringFromText: """
+            --------
+            --------
+            xooooooo
+            xxoooooo
+            xoxxoooo
+            -ooxoooo
+            --oooo--
+            --oooo--
+            """)
+        var state = State(board: board, turn: .dark, playerModes: [.manual, .manual])
+        state.diskCount[.dark] = 4
+        state.diskCount[.light] = 34
+        state.phase = AnyPhase(NextTurnPhase())
+        
+        let game = Game(state: state, middlewares: [Logger.self])
+        let gameState = game.statePublisher.share()
+
+        // ターンが白に変わって、ディスク枚数が更新されるが、
+        // 白は打つところがないので、Passフェーズに遷移する
+        let expectTurnChange = expectation(description: "Changed to light's turn")
+        let stateSubscriber1 = EventuallyFulfill<State, Never>(expectTurnChange, inputChecker: { state in
+            return state.turn == .light
+        })
+        gameState.subscribe(stateSubscriber1)
+        stateSubscriber1.store(in: &cancellables)
+
+        let expectDiskCount = expectation(description: "Disk-counts change")
+        let stateSubscriber2 = EventuallyFulfill<State, Never>(expectDiskCount, inputChecker: { state in
+            return state.diskCount[.dark] == 7 && state.diskCount[.light] == 32
+        })
+        gameState.subscribe(stateSubscriber2)
+        stateSubscriber2.store(in: &cancellables)
+
+        let expectPhaseToBeWaitForPlayer = expectation(description: "Phase transits to 'Pass'")
+        let stateSubscriber3 = EventuallyFulfill<State, Never>(expectPhaseToBeWaitForPlayer, inputChecker: { state in
+            return state.phase.kind == .pass
+        })
+        gameState.subscribe(stateSubscriber3)
+        stateSubscriber3.store(in: &cancellables)
+        
+        if let onEnter = NextTurnPhase.onEnter(previousPhase: AnyPhase(PlacingDiskPhase(cellChanges: [][...]))) {
+            game.dispatch(.thunk(onEnter))
+        }
+        wait(for: [expectTurnChange, expectDiskCount, expectPhaseToBeWaitForPlayer], timeout: 3.0)
+    }
+    
+    func testNextTurnPhase3() throws {
+        let board = try Board(restoringFromText: """
+            --------
+            --------
+            ----x---
+            ---xxx--
+            --xxxxx-
+            ---xxx--
+            ----x---
+            --------
+            """)
+        var state = State(board: board, turn: .dark, playerModes: [.manual, .manual])
+        state.diskCount[.dark] = 7
+        state.diskCount[.light] = 5
+        state.phase = AnyPhase(NextTurnPhase())
+        
+        let game = Game(state: state, middlewares: [Logger.self])
+        let gameState = game.statePublisher.share()
+
+        // ターンが白に変わって、ディスク枚数が更新されるが、
+        // 白は打つところがなく、黒も打てないので、GameOverフェーズに遷移する
+        let expectTurnChange = expectation(description: "Changed to light's turn")
+        let stateSubscriber1 = EventuallyFulfill<State, Never>(expectTurnChange, inputChecker: { state in
+            return state.turn == .light
+        })
+        gameState.subscribe(stateSubscriber1)
+        stateSubscriber1.store(in: &cancellables)
+
+        let expectDiskCount = expectation(description: "Disk-counts change")
+        let stateSubscriber2 = EventuallyFulfill<State, Never>(expectDiskCount, inputChecker: { state in
+            return state.diskCount[.dark] == 13 && state.diskCount[.light] == 0
+        })
+        gameState.subscribe(stateSubscriber2)
+        stateSubscriber2.store(in: &cancellables)
+
+        let expectPhaseToBeWaitForPlayer = expectation(description: "Phase transits to 'GameOver'")
+        let stateSubscriber3 = EventuallyFulfill<State, Never>(expectPhaseToBeWaitForPlayer, inputChecker: { state in
+            return state.phase.kind == .gameOver
+        })
+        gameState.subscribe(stateSubscriber3)
+        stateSubscriber3.store(in: &cancellables)
+        
+        if let onEnter = NextTurnPhase.onEnter(previousPhase: AnyPhase(PlacingDiskPhase(cellChanges: [][...]))) {
+            game.dispatch(.thunk(onEnter))
+        }
+        wait(for: [expectTurnChange, expectDiskCount, expectPhaseToBeWaitForPlayer], timeout: 3.0)
+    }
+}
